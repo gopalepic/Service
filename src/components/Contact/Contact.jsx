81c67d90d2bee2e17d6fc11c927295fe201bd40b
sumitsharma1436 <124554717+sumitsharma1436@users.noreply.github.com>
2024-06-08 23:04:08 -0700

    commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,72 +1,124 @@
 import React from 'react'
-import '../../index.css'
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer'
 const Contact = () => {
   return<>
   <Navbar/>
-  <div className="section contact" id="contact">
-        <div className="container">
-        <h1 style={{margin:"50px 40% 20px 40%"}}>Contact Us</h1>
-            <div className="row">
-                <div className="col-lg-8">
-                    <div className="contact-form-card">
-                        <h4 className="contact-title">Send a message</h4>
-                        <form action="">
-                            <div className="form-group">
-                                <input  className="form-control" type="text" placeholder="Name *" required/>
+  <section class="bg-blue-50 dark:bg-slate-800" id="contact">
+    <div class="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
+        <div class="mb-4">
+            <div class="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
+                {/* <h1 class="text-base font-semibold uppercase tracking-wide text-blue-600 dark:text-blue-200">
+                    Contact
+                </h1> */}
+                <h2
+                    class="font-heading mb-4 tracking-tight text-gray-900 dark:text-white text-3xl" style={{fontWeight:"900"}}>
+                    Get in Touch
+                </h2>
+            </div>
+        </div>
+        <div class="flex items-stretch justify-center">
+            <div class="grid md:grid-cols-2">
+                <div class="h-full pr-6">
+                    <p class="mt-3 mb-12 text-lg text-gray-600 dark:text-slate-400">
+                    </p>
+                    <ul class="mb-6 md:mb-0">
+                        <h1 style={{margin:"0 0 20px 0",fontSize:"22px",fontWeight:"800"}}>Contact Us</h1>
+                        <li class="flex">
+                            <div class="flex h-10 w-10 items-center justify-center rounded bg-black-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="h-6 w-6">
+                                    <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+                                    <path
+                                        d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z">
+                                    </path>
+                                </svg>
                             </div>
-                            <div className="form-group">
-                                <input className="form-control" type="email" placeholder="Email *" required/>
+                            <div class="ml-4 mb-4">
+                                <h3 class="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">Our Address
+                                </h3>
+                                <p class="text-gray-600 dark:text-slate-400"> Indrapuri Colony, Bhanwar Kuwa, Indore,
+                                </p>
+                                <p class="text-gray-600 dark:text-slate-400">Madhya Pradesh 452014</p>
                             </div>
-                            <div className="form-group">
-                                <textarea className="form-control" id="" placeholder="Message" rows="7" required></textarea>
+                        </li>
+                        <li class="flex">
+                            <div class="flex h-10 w-10 items-center justify-center rounded bg-black-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="h-6 w-6">
+                                    <path
+                                        d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2">
+                                    </path>
+                                    <path d="M15 7a2 2 0 0 1 2 2"></path>
+                                    <path d="M15 3a6 6 0 0 1 6 6"></path>
+                                </svg>
                             </div>
-                            <div className="form-group ">
-                                <button type="submit" className="form-control btn btn-primary" >Send Message</button>
+                            <div class="ml-4 mb-4">
+                                <h3 class="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">Contact
+                                </h3>
+                                {/* <p class="text-gray-600 dark:text-slate-400">*********</p> */}
+                                <p class="text-gray-600 dark:text-slate-400">info@kanthariyatechnologies.in</p>
                             </div>
-                        </form>
-                    </div>
+                        </li>
+                        <li class="flex">
+                            <div class="flex h-10 w-10 items-center justify-center rounded bg-black-900 ">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="h-6 w-6">
+                                    <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
+                                    <path d="M12 7v5l3 3"></path>
+                                </svg>
+                            </div>
+                            <div class="ml-4 mb-4">
+                                <h3 class="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">Working
+                                    hours</h3>
+                                <p class="text-gray-600 dark:text-slate-400">Monday - Friday: 10:00AM - 8:00PM</p>
+                                <p class="text-gray-600 dark:text-slate-400">Saturday &amp; Sunday: Off</p>
+                            </div>
+                        </li>
+                        <li className='flex'>
+                        <div className="flex items-center gap-4 mt-6">
+              <a href="#">
+                <FaInstagram className="text-4xl hover:text-primary duration-300 bg-black-300 ark:text-white" />
+              </a>
+              <a href="#">
+                <FaFacebook className="text-4xl hover:text-primary duration-300 bg-black-300 ark:text-white" />
+              </a>
+              <a href="#">
+                <FaLinkedin className="text-4xl hover:text-primary duration-300 bg-black-300 ark:text-white" />
+              </a>
+            </div>
+                        </li>
+                    </ul>
                 </div>
-                <div className="col-lg-4">
-                    <div className="contact-info-card">
-                        <h4 className="contact-title">Get in touch</h4>
-                        <div className="row mb-2">
-                            <div className="col-1 pt-1 mr-1">
-                                <i className="ti-mobile icon-md"></i>
+                <div class="card h-fit max-w-6xl p-5 md:p-12" id="form">
+                    <h2 class="mb-4 text-2xl font-bold dark:text-white">Ready to Get Started?</h2>
+                    <form id="contactForm">
+                        <div class="mb-6">
+                            <div class="mx-0 mb-1 sm:mb-4">
+                                <div class="mx-0 mb-1 sm:mb-4">
+                                    <label for="name" class="pb-1 text-xs uppercase tracking-wider"></label><input type="text" id="name" autocomplete="given-name" placeholder="Your name" class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0" name="name"/>
+                                </div>
+                                <div class="mx-0 mb-1 sm:mb-4">
+                                    <label for="email" class="pb-1 text-xs uppercase tracking-wider"></label><input type="email" id="email" autocomplete="email" placeholder="Your email address" class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0" name="email"/>
+                                </div>
                             </div>
-                            <div className="col-10 ">
-                                <h6 className="d-inline">Phone : <br/> <span className="text-muted">+91 9340782909</span></h6>
-                            </div>
-                        </div>
-                        <div className="row mb-2">
-                            <div className="col-1 pt-1 mr-1">
-                                <i className="ti-map-alt icon-md"></i>
-                            </div>
-                            <div className="col-10">
-                                <h6 className="d-inline">Address :<br/> <span className="text-muted">Indore</span></h6>
+                            <div class="mx-0 mb-1 sm:mb-4">
+                                <label for="textarea" class="pb-1 text-xs uppercase tracking-wider"></label><textarea id="textarea" name="textarea" cols="30" rows="5" placeholder="Write your message..." class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"></textarea>
                             </div>
                         </div>
-                        <div className="row mb-2">
-                            <div className="col-1 pt-1 mr-1">
-                                <i className="ti-envelope icon-md"></i>
-                            </div>
-                            <div className="col-10">
-                                <h6 className="d-inline">Email :<br/> <span className="text-muted">sumitshar2452@gmail.com</span></h6>
-                            </div>
+                        <div class="text-center">
+                            <button type="submit" class="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0">Send Message</button>
                         </div>
-                        <ul className="social-icons pt-4">
-                            <li className="social-item"><a className="social-link text-dark" href="#"><i className="ti-facebook" aria-hidden="true"></i></a></li>
-                            <li className="social-item"><a className="social-link text-dark" href="#"><i className="ti-twitter" aria-hidden="true"></i></a></li>
-                            <li className="social-item"><a className="social-link text-dark" href="#"><i className="ti-google" aria-hidden="true"></i></a></li>
-                            <li className="social-item"><a className="social-link text-dark" href="#"><i className="ti-instagram" aria-hidden="true"></i></a></li>
-                            <li className="social-item"><a className="social-link text-dark" href="#"><i className="ti-github" aria-hidden="true"></i></a></li>
-                        </ul> 
-                    </div>
+                    </form>
                 </div>
             </div>
-</div>
         </div>
+    </div>
+</section>
  <Footer/>
   </>
 
