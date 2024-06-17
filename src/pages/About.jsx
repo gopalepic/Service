235a543f196fadda5e86b4848956eb66c695ec0d
Gopal soni <Gopals.btech2022@iujaipur.edu.in>
2024-06-17 12:13:04 +0530

    made some changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -82,8 +82,8 @@ const About = () => {
         shadow-md bg-black opacity-80 z-1 absolute -my-[30%] m-[10%]
          '>
             <h1 className='text-6xl font-bold ml-3 text-[#8c1d54]'>We Are </h1>
-            <p className='p-1 md:text-xl md:p-2 lg:text-2xl lg:p-3'>
-
+            <p className='p-1 md:text-xl md:p-2 lg:text-2xl lg:p-3'>your trusted partner in IT solutions and services since 2020. As a proud member of the expansive Kanthariya Group, we are dedicated to delivering excellence and innovation across various industries, including textiles, mills, and automobiles.
+            </p>
 
                 </div>
  
@@ -124,6 +124,6 @@ const About = () => {
    <div className="bottom-0"> <Footer></Footer></div>
      </div>
   )
-}
 
+}
 export default About
