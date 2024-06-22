6e5f2eeedc492ec32c538ebbf702926c8f48d83e
Gopal soni <Gopals.btech2022@iujaipur.edu.in>
2024-06-22 14:24:24 +0530

    prototype-ready

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -89,26 +89,7 @@ const Footer = () => {
                 </ul>
               </div>
             </div>
-            {/* <div className="">
-              <div className="py-8 px-4 ">
-                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
-                  Help
-                </h1>
-                <ul className="flex flex-col gap-3">
-                  {HelpLinks.map((link) => (
-                    <li
-                      key={link.title}
-                      className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-400 "
-                      onClick={() => {
-                        navigate(link.link)
-                    }}
-                   >
-                      <span>{link.title}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div> */}
+            
             <div className="">
               <div className="py-8 px-4 ">
                 <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
